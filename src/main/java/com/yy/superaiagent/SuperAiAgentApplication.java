d4742e80462c4845778ddf19d749a51f1a498561
wyy <1802959495@qq.com>
2026-04-10 20:59:38 +0800

    项目初始化

---
+++ src/main/java/com/yy/superaiagent/SuperAiAgentApplication.java
@@ -0,0 +1,13 @@
+package com.yy.superaiagent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperAiAgentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SuperAiAgentApplication.class, args);
+    }
+
+}
