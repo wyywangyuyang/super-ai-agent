b59f422f635c0ce8182480adfbd8b7a8f3a3c630
wyy <1802959495@qq.com>
2026-04-15 15:48:49 +0800

    集中注册工具

---
+++ src/main/java/com/yy/superaiagent/tools/ToolRegistration.java
@@ -0,0 +1,38 @@
+package com.yy.superaiagent.tools;
+
+import org.springframework.ai.support.ToolCallbacks;
+import org.springframework.ai.tool.ToolCallback;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * 集中注册工具
+ *
+ * @author wyy
+ */
+@Configuration
+public class ToolRegistration {
+
+    @Value("${search-api.api-key}")
+    private String searchApiKey;
+
+    @Bean
+    public ToolCallback[] allTools() {
+        FileOperationTool fileOperationTool = new FileOperationTool();
+        WebSearchTool webSearchTool = new WebSearchTool(searchApiKey);
+        WebScrapingTool webScrapingTool = new WebScrapingTool();
+        TerminalOperationTool terminalOperationTool = new TerminalOperationTool();
+        ResourceDownloadTool resourceDownloadTool = new ResourceDownloadTool();
+        PDFGenerationTool pdfGenerationTool = new PDFGenerationTool();
+        return ToolCallbacks.from(
+                fileOperationTool,
+                webSearchTool,
+                webScrapingTool,
+                terminalOperationTool,
+                resourceDownloadTool,
+                pdfGenerationTool
+        );
+    }
+
+}
