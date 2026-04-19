b6c38a17f1c1c81ca790b3a1196c43af8e479cd1
wyy <1802959495@qq.com>
2026-04-19 21:18:54 +0800

    解决跨域问题

---
+++ src/main/java/com/yy/superaiagent/config/CorsConfig.java
@@ -0,0 +1,27 @@
+package com.yy.superaiagent.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * 解决跨域问题
+ *
+ * @author wyy
+ */
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        // 覆盖所有请求
+        registry.addMapping("/**")
+                // 允许发送 Cookie
+                .allowCredentials(true)
+                // 放行哪些域名（必须用patterns，否则*会和allowCredentials冲突）
+                .allowedOriginPatterns("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .exposedHeaders("*");
+    }
+}
